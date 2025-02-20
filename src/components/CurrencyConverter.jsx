b82061b62dd5f0066372abe1b0a43b116bfbe5f0
Espadv69 <aitorjob69@gmail.com>
2025-02-20 10:43:03 +0100

    Implement state management for amount and currency selection in CurrencyConverter component

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import { exchangeRates } from '../data/exchangeRates'
 
-const CurrencyConverter = () => {}
+const CurrencyConverter = () => {
+  const [amount, setAmount] = useState(1)
+  const [fromCurrency, setFromCurrency] = useState('USD')
+  const [toCurrency, setToCurrency] = useState('EUR')
+  const [convertedAmount, setConvertedAmount] = useState(0)
+}
 
 export default CurrencyConverter
