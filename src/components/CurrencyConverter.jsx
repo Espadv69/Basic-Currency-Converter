eb5c9c37cb13b976c5a5cebaabec80bf8a4b52ca
Espadv69 <aitorjob69@gmail.com>
2025-02-20 10:49:18 +0100

    Add currency conversion logic to CurrencyConverter component

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -6,6 +6,15 @@ const CurrencyConverter = () => {
   const [fromCurrency, setFromCurrency] = useState('USD')
   const [toCurrency, setToCurrency] = useState('EUR')
   const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const convertCurrency = () => {
+    if (fromCurrency === toCurrency) {
+      setConvertedAmount(amount)
+    } else {
+      const rate = exchangeRates[fromCurrency][toCurrency] || 1
+      setConvertedAmount(amount * rate)
+    }
+  }
 }
 
 export default CurrencyConverter
